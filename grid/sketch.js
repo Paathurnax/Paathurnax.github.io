22ddd83ff0c822c37af8da571f057642f46d8c68
Jacob <165198998+Paathurnax@users.noreply.github.com>
2024-10-23 13:56:42 -0600

    finished demo

--- grid/sketch.js
+++ grid/sketch.js
@@ -9,8 +9,9 @@
 
 let grid;
 
-const GRID_SIZE = 4;
+const GRID_SIZE = 10;
 let cellSize;
+let shouldToggleNeighbours = true;
 
 function setup() {
   if (windowWidth < windowHeight) {
@@ -28,6 +29,44 @@ function draw() {
   displayGrid();
 }
 
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  toggleCell(x, y);
+
+  if (shouldToggleNeighbours === true) {
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
+
+  }
+}
+
+function toggleCell(x, y) {
+  //bs check
+  if(x>=0 && y>=0 && x<GRID_SIZE && y<GRID_SIZE){
+    if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+    else {
+      grid[y][x] = 1;
+    }
+
+  }
+}
+
+function windowResized() {
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
+}
+
 function keyPressed() {
   if (key === "r") {
     grid = generateRandGrid(GRID_SIZE, GRID_SIZE);
@@ -35,14 +74,17 @@ function keyPressed() {
   if (key === "e") {
     grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
   }
+  if (key === "n") {
+    shouldToggleNeighbours = !shouldToggleNeighbours;
+  }
 }
   
 
 function generateRandGrid(cols, rows) {
   let newGrid = [];
-  for (let y = 0; y<cols; y++) {
+  for (let y = 0; y<rows; y++) {
     newGrid.push([]);
-    for (let x = 0; x<rows; x++) {
+    for (let x = 0; x<cols; x++) {
       //choose either 0 or 1 each 50% chance
       if (random(100) < 50) {
         newGrid[y].push(1);
@@ -71,19 +113,15 @@ function displayGrid() {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
       if (grid[y][x] === 1) {
-        fill("black");
+        fill("red");
       }
 
       else if (grid[y][x] === 0) {
-        fill("white");
+        fill("blue");
       }
 
       square(x*cellSize, y*cellSize, cellSize);
       
     }
   }
-}
-
-function mousePressed() {
-  
 }
