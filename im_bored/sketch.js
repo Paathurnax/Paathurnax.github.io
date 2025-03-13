2b291b31bf578f36a8c79be64425a4f5f5b9e444
Jacob <165198998+Paathurnax@users.noreply.github.com>
2025-03-13 11:15:13 -0600

    got more bored

--- im_bored/sketch.js
+++ im_bored/sketch.js
@@ -6,21 +6,48 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let ball = new Ball;
 }
 
 function draw() {
   background(220);
-  multipleSquare(width/50)
+  ball.functions();
 }
 
-function makeSquare(x, y, width, height) {
-  rect(x, y, width, height)
-}
+class Ball {
+  constructor() {
+    this.x = width/2;
+    this.y = height/2;
+    this.xspeed = 10;
+    this.yspeed = 15;
+    this.radius = 10;
+  }
 
-function multipleSquare(i) {
-  for (let j = 0; j<i; j++) {
-    makeSquare(j*50, height/2 - 25, 50, 50)
+  makeBall() {
+    circle(this.x, this.y, this.radius*2);
+  }
+
+  moveBall() {
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+  }
+
+  bounce() {
+    if (this.x >= width - this.radius || this.x < this.radius) {
+      this.xspeed *= -1;
+    }
+
+    if (this.y >= height - this.radius || this.y < this.radius) {
+      this.yspeed *= -1;
+    }
+  }
+
+  functions() {
+    this.makeBall();
+    this.moveBall();
+    this.bounce();
   }
 }
