a81b588ac26eb6707591f9847182f4fc14142ce8
Jacob <165198998+Paathurnax@users.noreply.github.com>
2025-05-19 20:08:46 -0600

    i wanted to recreate a python script in javascript, but i had quickly realized that i had kinda forgotten how to code.

--- final_project/sketch.js
+++ final_project/sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +13,12 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function removeVowels(str) {
+  for (let i = 0; i < length(str); i++) {
+    if (str[i] in vowels) {
+      
+    }
+  }
 }
